59e4819ab64f80d0c0c92b1706754302f2fe0ba1
Kelyt07 <bca59818@gmail.com>
2025-11-04 07:55:39 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Laboratoire prêt !")
+const motDePasseAttendu = "secret123";
+let motDePasseUtilisateur = "123456789"; // Changez cette valeur pour tester
+
+if (motDePasseAttendu === motDePasseUtilisateur) {
+    console.log("Accès autorisé. Bienvenue !");
+} else if (motDePasseUtilisateur.length< 9) {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
