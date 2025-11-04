7dc16961dc83a247f3b23eac9d17c653de3242cc
Kelyt07 <bca59818@gmail.com>
2025-11-04 08:18:30 +0000

    feat: Maîtrise des Objets JS

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const motDePasseAttendu = "secret123";
-let motDePasseUtilisateur = "123456789"; // Changez cette valeur pour tester
-
-if (motDePasseAttendu === motDePasseUtilisateur) {
-    console.log("Accès autorisé. Bienvenue !");
-} else if (motDePasseUtilisateur.length< 9) {
-    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
-} else {
-    console.log("Accès refusé. Mot de passe incorrect.");
-}
+const moi = {
+    prenom: "Kelite",
+    nom: "Bonnamour",
+    age: 19,
+    competences: ["HTML", "CSS"]
+};
+moi.ville = "Paris";
+console.log("je m'appel", moi.prenom, moi.nom); // Affiche "VotrePrénom"
+console.log(moi);
